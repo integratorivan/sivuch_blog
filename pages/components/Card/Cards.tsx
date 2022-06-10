3a81110f0f9e3451d5ea5549abcd32081e17d458
Ivan Viktorov <i.viktorov@platform.live>
2022-06-10 14:22:46 +0300

    delete all

---
+++ pages/components/Card/Cards.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@nextui-org/react";
+import React from 'react';
+
+const Card = () => {
+    return (
+        <Card css={{ mw: "400px" }}>
+            <Card.Body>
+                <Text>A basic card</Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Card;
